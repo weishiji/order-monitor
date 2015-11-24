c666f9c2e76b91cc7eacdf4e3edc51ed1e784f90
lxg <liuxiaoguang@chicv.com>
2015-11-25 00:26:27 +0800

    渲染衣服销量的前五位

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -6,61 +6,79 @@ require.config({
       echarts: '/bower_components/echarts/build/dist'
   }
 })
-function resizeCanvas() {
-	$canvas.each(function(i,can){
-		can.style.width = window.innerWidth/2 + "px";
-	  // artifical delay so innerHeight is correct
-	  setTimeout(function() {
-	    can.style.height = window.innerHeight/2 + "px";
-	  }, 0);	
-	});
-};
-resizeCanvas();
-window.onresize = resizeCanvas;
 
-
-
-socket.on('news', function (data) {
-  console.log(data);
-  socket.emit('my other event', { my: 'data' });
-});	
 
 require([
 	'echarts'
 	,'echarts/chart/bar' // 使用柱状图就加载bar模块，按需加载
+	,'echarts/chart/line'
 ],function(ec){
 	var myChart = ec.init(document.getElementById('order-average')); 
-	 var option = {
-      tooltip: {
-          show: true
-      },
-      legend: {
-          data:['销量']
-      },
-      xAxis : [
-          {
-              type : 'category',
-              data : ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
-          }
-      ],
-      yAxis : [
-          {
-              type : 'value'
-          }
-      ],
-      series : [
-          {
-              "name":"销量",
-              "type":"bar",
-              "data":[5, 20, 40, 10, 10, 20]
-          }
-      ]
+ 	var option = {
+    tooltip: {
+      show: true
+    },
+    legend: {
+      data:['畅销商品']
+    },
+    xAxis : [{
+      type : 'category',
+      data : undefined
+    }],
+    yAxis : [{
+      type : 'value'
+    }],
+    series : [{
+      "name":"销量",
+      "type":"bar",
+      "data":undefined
+    }]
   };
-  // 为echarts对象加载数据 
-  myChart.setOption(option); 
-  setInterval(function(){
-  	option.series[0].data[0] += 1
-  	console.log(option)
-  	myChart.setOption(option)
-  },1000)
+  option = {
+    title : {
+      text: '衣服销量前五位',
+      subtext: 'Power By Wangchi'
+    },
+    tooltip : {
+      trigger: 'axis'
+    },
+    legend: {
+      data:[new Date()]
+    },
+    toolbox: {
+      show : true,
+      feature : {
+          restore : {show: true},
+          saveAsImage : {show: true}
+      }
+    },
+    calculable : true,
+    xAxis : [{
+      type : 'value'
+    }],
+    yAxis : [{
+			type : 'category',
+      data : undefined
+    }],
+    series : [{
+      name:'销量',
+      type:'bar',
+      data:undefined
+    }]
+};
+                    
+	socket.on('sales', function (data) {
+	  console.log(data);
+	  option.yAxis[0].data = [];
+	  option.series[0].data = [];
+	 	for(var i=0;i<data.length;i+=1){
+	 		var temp = data[i];
+	 		option.yAxis[0].data.push(temp.product_id);
+	 		option.series[0].data.push(temp.total_quantity);
+	 	}
+	  // 为echarts对象加载数据 
+	 	myChart.setOption(option);
+	});	
+  
+
 })
--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,17 @@ var exec = require('../server/db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	exec('select * from oc_product where product_id=?',[100],function(err,rows){
-		console.log(rows)
-		io.on('connection', function (socket) {
-		  socket.emit('news', { hello : rows });
+	var sql = 'select count(*) as order_count,sum(op.quantity) as total_quantity,' + 
+		' op.product_id as product_id,pd.name as product_name' + 
+    ' from oc_order_product op' +
+		' inner join oc_paypal_order po on op.order_id = po.order_id and DATE(convert_tz(po.date_added, "UTC", "+8:00"))= ?' + 
+    ' inner join oc_product_description pd on pd.product_id = op.product_id' + 
+    ' group by op.product_id' + 
+    ' order by total_quantity desc' + 
+    ' limit 5;'
+	exec(sql,['2015-11-19'],function(err,rows){
+		io.on('connection',function (socket) {
+		  socket.emit('sales', rows);
 		  socket.on('my other event', function (data) {
 		    console.log(data);
 		  });
