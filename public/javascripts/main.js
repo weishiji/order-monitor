5fe7e4655c3db40ae8b1a593a9c9709155ce4297
lxg <liuxiaoguang@chicv.com>
2015-11-24 16:50:27 +0800

    引入百度的echart 替换chart 百度的好用一些

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,8 +1,11 @@
 var socket = io('http://localhost:3000');
 var $canvas = $('canvas');
 
-var orderNumCtx = $canvas.get(0).getContext('2d')
-	,orderPriceCtx = $canvas.get(1).getContext('2d')
+require.config({
+	paths: {
+      echarts: '/bower_components/echarts/build/dist'
+  }
+})
 function resizeCanvas() {
 	$canvas.each(function(i,can){
 		can.style.width = window.innerWidth/2 + "px";
@@ -22,25 +25,42 @@ socket.on('news', function (data) {
   socket.emit('my other event', { my: 'data' });
 });	
 
-var data = {
-	labels : ["January","February","March","April","May","June","July"],
-	datasets : [
-		{
-			fillColor : "rgba(220,220,220,0.5)",
-			strokeColor : "rgba(220,220,220,1)",
-			pointColor : "rgba(220,220,220,1)",
-			pointStrokeColor : "#fff",
-			data : [65,59,90,81,56,55,40]
-		},
-		{
-			fillColor : "rgba(151,187,205,0.5)",
-			strokeColor : "rgba(151,187,205,1)",
-			pointColor : "rgba(151,187,205,1)",
-			pointStrokeColor : "#fff",
-			data : [28,48,40,19,96,27,100]
-		}
-	]
-}
-
-
-new Chart(orderNumCtx).Line(data);
+require([
+	'echarts'
+	,'echarts/chart/bar' // 使用柱状图就加载bar模块，按需加载
+],function(ec){
+	var myChart = ec.init(document.getElementById('order-average')); 
+	 var option = {
+      tooltip: {
+          show: true
+      },
+      legend: {
+          data:['销量']
+      },
+      xAxis : [
+          {
+              type : 'category',
+              data : ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+          }
+      ],
+      yAxis : [
+          {
+              type : 'value'
+          }
+      ],
+      series : [
+          {
+              "name":"销量",
+              "type":"bar",
+              "data":[5, 20, 40, 10, 10, 20]
+          }
+      ]
+  };
+  // 为echarts对象加载数据 
+  myChart.setOption(option); 
+  setInterval(function(){
+  	option.series[0].data[0] += 1
+  	console.log(option)
+  	myChart.setOption(option)
+  },1000)
+})
