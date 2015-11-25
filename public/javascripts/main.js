0a18911e8c50bebd8cbbc23edf6704985e3a5682
lxg <liuxiaoguang@chicv.com>
2015-11-26 02:14:23 +0800

    引入count up js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -36,7 +36,7 @@ require([
   };
   option = {
     title : {
-      text: '衣服销量前五位',
+      text: '设计师销量前五位',
       subtext: 'Power By Wangchi'
     },
     tooltip : {
@@ -73,8 +73,8 @@ require([
 	  option.series[0].data = [];
 	 	for(var i=0;i<data.length;i+=1){
 	 		var temp = data[i];
-	 		option.yAxis[0].data.push(temp.product_id);
-	 		option.series[0].data.push(temp.total_quantity);
+	 		option.yAxis[0].data.push(temp.name);
+	 		option.series[0].data.push(temp.m_total);
 	 		myChart.setOption(option);
 	 	}
 	 	
@@ -85,87 +85,103 @@ require([
 	,'echarts/chart/gauge' // 使用仪表盘就加载gauge模块，按需加载
 ],function(ec){
 	var myChart = ec.init(document.getElementById('gauge')); 
-	option = {
-    tooltip : {
-      formatter: "{a} <br/>{b} : {c}%"
-    },
-    title : {
-      text: '总订单（单位:万$）',
-      subtext: 'Power By Wangchi'
-    },
-    toolbox: {
-      show : true,
-      feature : {
-        mark : {show: true},
-        restore : {show: true},
-        saveAsImage : {show: true}
-      }
-    },
-    series : [{
-      name:'业务指标',
-      type:'gauge',
-      startAngle: 180,
-      endAngle: 0,
-      center : ['50%', '99%'],    // 默认全局居中
-      radius : 300,
-      axisLine: {            // 坐标轴线
-        lineStyle: {       // 属性lineStyle控制线条样式
-          width: 200
-        }
+  var option = {
+      tooltip: {
+          formatter: "{a} <br/>{b} : {c}%"
       },
-      axisTick: {            // 坐标轴小标记
-        splitNumber: 10,   // 每份split细分多少段
-        length :12,        // 属性length控制线长
+      title: {
+          text: '总订单（单位:万$）',
+          subtext: 'Power By Wangchi'
       },
-      axisLabel: {           // 坐标轴文本标签，详见axis.axisLabel
-        formatter: function(v){
-          switch (v+''){
-              case '20' : return '6';
-              case '50': return '15';
-              case '80': return '24';
-              case '100' : return '30';
-              default: return '';
-          }
-        },
-        textStyle: {       // 其余属性默认使用全局文本样式，详见TEXTSTYLE
-            color: '#fff',
-            fontSize: 15,
-            fontWeight: 'bolder'
-        }
-	    },
-      pointer: {
-        width:50,
-        length: '90%',
-        color: 'rgba(255, 255, 255, 0.8)'
-      },
-      title : {
-          show : true,
-          offsetCenter: [0, '-60%'],       // x, y，单位px
-          textStyle: {       // 其余属性默认使用全局文本样式，详见TEXTSTYLE
-              color: '#fff',
-              fontSize: 30
+      toolbox: {
+          show: true,
+          feature: {
+              mark: {
+                  show: true
+              },
+              restore: {
+                  show: true
+              },
+              saveAsImage: {
+                  show: true
+              }
           }
       },
-      detail : {
-          show : true,
-          backgroundColor: 'rgba(0,0,0,0)',
-          borderWidth: 0,
-          borderColor: '#ccc',
-          width: 100,
-          height: 40,
-          offsetCenter: [0, -40],       // x, y，单位px
-          formatter:'{value}%',
-          textStyle: {       // 其余属性默认使用全局文本样式，详见TEXTSTYLE
-              fontSize : 50
-          }
-      },
-      data:[{value: 50, name: '完成率'}]
-  }]
-};
+      series: [{
+          name: '业务指标',
+          type: 'gauge',
+          startAngle: 180,
+          endAngle: 0,
+          center: ['50%', '99%'], // 默认全局居中
+          radius: 300,
+          axisLine: { // 坐标轴线
+              lineStyle: { // 属性lineStyle控制线条样式
+                  width: 200
+              }
+          },
+          axisTick: { // 坐标轴小标记
+              splitNumber: 10, // 每份split细分多少段
+              length: 12, // 属性length控制线长
+          },
+          axisLabel: { // 坐标轴文本标签，详见axis.axisLabel
+              formatter: function(v) {
+                  switch (v + '') {
+                      case '20':
+                          return '6';
+                      case '50':
+                          return '15';
+                      case '80':
+                          return '24';
+                      case '100':
+                          return '30';
+                      default:
+                          return '';
+                  }
+              },
+              textStyle: { // 其余属性默认使用全局文本样式，详见TEXTSTYLE
+                  color: '#fff',
+                  fontSize: 15,
+                  fontWeight: 'bolder'
+              }
+          },
+          pointer: {
+            width: 50,
+            length: '90%',
+            color: 'rgba(255, 255, 255, 0.8)'
+          },
+          title: {
+            show: true,
+            offsetCenter: [0, '-60%'], // x, y，单位px
+            textStyle: { // 其余属性默认使用全局文本样式，详见TEXTSTYLE
+                color: '#fff',
+                fontSize: 30
+            }
+          },
+          detail: {
+            show: true,
+            backgroundColor: 'rgba(0,0,0,0)',
+            borderWidth: 0,
+            borderColor: '#ccc',
+            width: 100,
+            height: 40,
+            offsetCenter: [0, -40], // x, y，单位px
+            formatter: '{value}%',
+            textStyle: { // 其余属性默认使用全局文本样式，详见TEXTSTYLE
+                fontSize: 50
+            }
+          },
+          data: [{
+              value: 50,
+              name: '完成率'
+          }]
+      }]
+  };
+
 	socket.on('soldAll',function(data){
 		var all = data.sold_total;
 		option.series[0].data[0].value = (all / 300000 * 100) .toFixed(2) - 0;
     myChart.setOption(option,true);	
+
 	})
    	       
 })
--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,7 @@ function getSocket(fun){
 /* GET home page. */
 router.get('/', function(req, res, next) {
   function sendSellWellData(socket){
+  	/*
   	var sql = 'select count(*) as order_count,sum(op.quantity) as total_quantity,' + 
 			' op.product_id as product_id,pd.name as product_name' + 
 	    ' from oc_order_product op' +
@@ -21,6 +22,19 @@ router.get('/', function(req, res, next) {
 	    ' group by op.product_id' + 
 	    ' order by total_quantity desc' + 
 	    ' limit 5;'
+    */
+    var sql = 'SELECT' +
+	    ' m.name as name,' +
+	    ' round(sum(op.total), 2) AS m_total' +
+			' FROM oc_order o' +
+	    ' INNER JOIN oc_paypal_order po' +
+      ' ON (o.order_id = po.order_id AND date(convert_tz(po.date_added, "utc", "+8:00")) = ?)' +
+	    ' INNER JOIN oc_order_product op ON (o.order_id = op.order_id)' +
+	    ' INNER JOIN oc_product p ON (op.product_id = p.product_id)' +
+	    ' INNER JOIN oc_manufacturer m ON (p.manufacturer_id = m.manufacturer_id)' +
+			' GROUP BY m.manufacturer_id' +
+			' ORDER BY m_total DESC' +
+			' LIMIT 5;' 
   	exec(sql,['2015-11-19'],function(err,rows){
   		socket.emit('sellWell', rows);
   		setTimeout(function(){
