d5be2fd6c949035cad738733dab476abad7cc377
lxg <liuxiaoguang@chicv.com>
2015-11-25 13:56:42 +0800

    增加仪表盘的控件，

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -82,6 +82,91 @@ require([
 	 	},10000)
 	 	
 	});	
-  
+})
+require([
+	'echarts'
+	,'echarts/chart/gauge' // 使用仪表盘就加载gauge模块，按需加载
+],function(ec){
+	var myChart = ec.init(document.getElementById('gauge')); 
+	option = {
+    tooltip : {
+      formatter: "{a} <br/>{b} : {c}%"
+    },
+    title : {
+      text: '总订单（单位:万$）',
+      subtext: 'Power By Wangchi'
+    },
+    toolbox: {
+      show : true,
+      feature : {
+        mark : {show: true},
+        restore : {show: true},
+        saveAsImage : {show: true}
+      }
+    },
+    series : [{
+      name:'业务指标',
+      type:'gauge',
+      startAngle: 180,
+      endAngle: 0,
+      center : ['50%', '99%'],    // 默认全局居中
+      radius : 300,
+      axisLine: {            // 坐标轴线
+        lineStyle: {       // 属性lineStyle控制线条样式
+          width: 200
+        }
+      },
+      axisTick: {            // 坐标轴小标记
+        splitNumber: 10,   // 每份split细分多少段
+        length :12,        // 属性length控制线长
+      },
+      axisLabel: {           // 坐标轴文本标签，详见axis.axisLabel
+        formatter: function(v){
+        	console.log()
+          switch (v+''){
+              case '20' : return '6';
+              case '50': return '15';
+              case '80': return '24';
+              case '100' : return '30';
+              default: return '';
+          }
+        },
+        textStyle: {       // 其余属性默认使用全局文本样式，详见TEXTSTYLE
+            color: '#fff',
+            fontSize: 15,
+            fontWeight: 'bolder'
+        }
+	    },
+      pointer: {
+        width:50,
+        length: '90%',
+        color: 'rgba(255, 255, 255, 0.8)'
+      },
+      title : {
+          show : true,
+          offsetCenter: [0, '-60%'],       // x, y，单位px
+          textStyle: {       // 其余属性默认使用全局文本样式，详见TEXTSTYLE
+              color: '#fff',
+              fontSize: 30
+          }
+      },
+      detail : {
+          show : true,
+          backgroundColor: 'rgba(0,0,0,0)',
+          borderWidth: 0,
+          borderColor: '#ccc',
+          width: 100,
+          height: 40,
+          offsetCenter: [0, -40],       // x, y，单位px
+          formatter:'{value}%',
+          textStyle: {       // 其余属性默认使用全局文本样式，详见TEXTSTYLE
+              fontSize : 50
+          }
+      },
+      data:[{value: 50, name: '完成率'}]
+  }]
+};
 
+myChart.setOption(option,true);
+                    
 })
