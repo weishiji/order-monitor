989ab754d580fd74cbdb536601cee53625778a87
lxg <liuxiaoguang@chicv.com>
2015-11-26 14:05:43 +0800

    bug fixed

--- routes/index.js
+++ routes/index.js
@@ -31,7 +31,7 @@ router.get('/', function(req, res, next) {
 	    ' round(sum(op.total), 2) AS m_total' +
 			' FROM oc_order o' +
 	    ' INNER JOIN oc_paypal_order po' +
-      ' ON (o.order_id = po.order_id AND date(convert_tz(po.date_added, "utc", "+8:00")) = ?)' +
+      ' ON (o.order_id = po.order_id AND date(convert_tz(po.date_added, "+0:00", "+8:00")) = ?)' +
 	    ' INNER JOIN oc_order_product op ON (o.order_id = op.order_id)' +
 	    ' INNER JOIN oc_product p ON (op.product_id = p.product_id)' +
 	    ' INNER JOIN oc_manufacturer m ON (p.manufacturer_id = m.manufacturer_id)' +
@@ -47,7 +47,7 @@ router.get('/', function(req, res, next) {
   }
   function sendSoldAllData(socket){
   	var sql = 'select sum(po.total) as sold_total from oc_paypal_order as po' + 
-			' where DATE(convert_tz(po.date_added, "UTC", "+8:00"))=? limit 1;'
+			' where DATE(convert_tz(po.date_added, "+0:00", "+8:00"))=? limit 1;'
 		exec(sql,[currentDate],function(err,rows){
 			socket.emit('soldAll',rows[0]);
 			setTimeout(function(){
@@ -62,7 +62,7 @@ router.get('/', function(req, res, next) {
 	    ' round(sum(op.total), 2) AS p_total' + 
 			' FROM oc_order o' + 
 	    ' INNER JOIN oc_paypal_order po' + 
-	    ' ON (o.order_id = po.order_id AND date(convert_tz(po.date_added, "utc", "+8:00")) = ?)' + 
+	    ' ON (o.order_id = po.order_id AND date(convert_tz(po.date_added, "+0:00", "+8:00")) = ?)' + 
 	    ' INNER JOIN oc_order_product op ON (o.order_id = op.order_id)' + 
 	    ' INNER JOIN oc_product_description pd on (op.product_id=pd.product_id)' + 
 			' GROUP BY op.product_id' + 
