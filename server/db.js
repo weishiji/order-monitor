68237b267d56a6ed7fb2ff3168e59fa700955567
shiyongyong <shiyongyong@chicv.com>
2017-12-01 16:29:53 +0800

    fixed

--- server/db.js
+++ server/db.js
@@ -1,30 +1,13 @@
 var mysql = require('mysql');
 var config = require('../config');
-
-function _exec(sqls,values,after){
+config.db.connectionLimit = 10;
+var pool  = mysql.createPool(config.db);
+function _exec(sqls,values){
     
-    var client = mysql.createConnection(config.db);
     var cb = function(err,rs,fields){
-        //client.end();
-            return values(err,rs,fields)
+        if (err) throw err;
+        return values(err,rs,fields)
     }
-    var query = client.query(sqls || '', cb || [],function(err,r){          
-        after(err,r);
-        client.end();
-    });
-
-    //console.log(query.sql)
-    // client.end(function(err){
-    //     console.log(err,'this is error')
-    // });
-
-    client.on('error',function(err) {
-        if (err.errno != 'ECONNRESET') {
-            after("err01",false);
-            throw err;
-        } else {
-            after("err02",false);
-        }
-    });
+    pool.query(sqls, cb);
 }
 module.exports = _exec;
